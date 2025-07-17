009345d758b11071bc2a250ae8ffabcf505b4d06
Dhyey <dsitapara@gmail.com>
2025-07-17 17:08:51 +0530

    image

--- lib/main.dart
+++ lib/main.dart
@@ -71,11 +71,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(,child: Container(
-        
-        height: 100,
-        width: 100,
-        child: Image.asset("assets/images/hi.png"))),
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          child: Image.asset("assets/images/hi.png"),
+        ),
+      ),
     );
   }
 }
